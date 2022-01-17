7ce6ea545d7f3613a9e21be7f0c8b3d8db86fe81
Nielsen Pierce <nielsenpi@softcat.com>
2022-01-17 11:29:01 +0000

    Update README and examples

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -7,3 +7,11 @@ module "tagging" {
   softcat_build_type  = "Greenfield"
 }
 
+
+resource "azurerm_resource_group" "rg_softcat_taggig" {
+  name     = "rg-softcat-tagging"
+  location = "UK South"
+
+  tags = module.tagging.softcat_tags
+
+}
