bf48aa6988afd5da95e0888fdfdc2c6b11cd31f0
Nielsen Pierce <nielsenpi@softcat.com>
2022-03-21 10:33:12 +0000

    Test tag overwrite

--- tests/basic/main.tf
+++ tests/basic/main.tf
@@ -7,10 +7,24 @@ module "tagging" {
 }
 
 
-resource "azurerm_resource_group" "rg-softcat-tag-test-basic" {
+resource "azurerm_resource_group" "rg-softcat-tag-test1-basic" {
   name     = "rg-softcat-tag-test-basic"
   location = "UK South"
 
   tags = module.tagging.softcat_tags
-
 }
+
+
+resource "azurerm_resource_group" "rg-softcat-tag-test2-basic" {
+  name     = "rg-softcat-tag-test2-basic"
+  location = "UK South"
+
+  tags = merge(module.tagging.softcat_tags,
+    {
+      Softcat_Environment = "Overwrite"
+    }
+  )
+}
+
+
+
