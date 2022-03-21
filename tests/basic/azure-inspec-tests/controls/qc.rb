430acceb8f58be1184cb1672eb6afd3bbcd708ee
Nielsen Pierce <nielsenpi@softcat.com>
2022-03-21 13:56:51 +0000

    Move testing azure pipelines

--- tests/basic/azure-inspec-tests/controls/qc.rb
+++ tests/basic/azure-inspec-tests/controls/qc.rb
@@ -1,8 +1,16 @@
 # Test values
 
-resource_group1 = "rg-softcat-tag-test-basic"
+resource_group1 = "rg-softcat-tag-test1-basic"
+resource_group2 = "rg-softcat-tag-test2-basic"
 
 describe azure_resource_group(name: resource_group1) do
+  its('tags') { should include(:Softcat_Build_Type) }
+  its('tags') { should include(:Softcat_Managed) }
+  its('tags') { should include(:Softcat_Environment) }
+end
+
+
+describe azure_resource_group(name: resource_group2) do
   its('tags') { should include(:Softcat_Build_Type) }
   its('tags') { should include(:Softcat_Managed) }
   its('tags') { should include(:Softcat_Environment) }
--- tests/basic/provider.tf
+++ tests/basic/provider.tf
@@ -2,12 +2,12 @@ terraform {
   required_version = ">=1.0.0"
 
   # Azurerm Backend https://www.terraform.io/docs/language/settings/backends/azurerm.html#
-  # backend "azurerm" {
-  #   resource_group_name  = "Terraform-Storage-accountRG"
-  #   storage_account_name = "softcattfmodulestate"
-  #   container_name       = "tf-modules-azure-softcat-tagging-basic"
-  #   key                  = "terraform.tfstate"
-  # }
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-modules-state"
+    storage_account_name = "softcatmodulestate"
+    container_name       = "tf-modules-azure-softcat-tagging-basic"
+    key                  = "terraform.tfstate"
+  }
 
   required_providers {
     # Uncomment required providers and delete unneeded
