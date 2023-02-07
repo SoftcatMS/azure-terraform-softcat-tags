4278a4ba8f70932633d49812b5c69772e252c9a7
Nielsen Pierce <nielsenpi@softcat.com>
2023-02-07 16:10:29 +0000

    Add softcat_monitoring tag

--- main.tf
+++ main.tf
@@ -3,5 +3,6 @@ locals {
     softcat_managed_type = var.softcat_managed_type
     softcat_environment  = var.softcat_environment
     softcat_build_type   = var.softcat_build_type
+    softcat_monitoring   = var.softcat_monitoring
   })
 }
